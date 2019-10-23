32e7297a680b1cb6434a96a2ca92fe8b446e97b9
rconnors96 <rconnors96@comcast.net>
2019-10-22 23:05:45 -0400

    Add exisiting file

--- kernel.c
+++ kernel.c
@@ -1,9 +1,13 @@
 void printChar(char); //printChar function declaration
 void printString(char*); //printString function declaration
+void readString(char*); //readString function declaration
 
 void main() {
 
-	printChar('F');
+	char line[80];
+	printString("Enter a line: ");
+	readString(line);
+	printString(line);
 
 
 	while(1);
@@ -16,5 +20,36 @@ void printChar(char c) {
 
 void printString(char* chars) {
 	int i=0;
+	while (chars[i] != 0x0) {
+		printChar(chars[i]);
+		i++;
+	}
 }
 
+void readString(char* chars) {
+	int i = 0;
+	int run = 1;
+	while (run == 1) {
+		char current = interrupt(0x16,0,0,0,0); //sets user-entered char to current
+		if (current != 0xd) { //checks if char is "Enter"
+			if (current == 0x8) { //checks if char is "Backspace"
+				printChar(' ');
+				printChar(0x8);
+				if (i > 0) { //decreases i index if it is greater than 0
+					i--; //this ensures index doesnt go negative
+				}
+			}
+			chars[i] = current; //stores the char in an array
+			printChar(current); //prints the typed character
+			i++; //increases char array index
+		}
+		else {
+			chars[i] = 0xa; //adds line feed char to array
+			printChar(0xa); //prints line feed
+			i++;
+			chars[i] = 0x0; //adds end of string char to array
+			printChar(0x0); //prints end of string char
+			run = 0; //turns off the while loop that checks for input
+		}
+	}
+}
