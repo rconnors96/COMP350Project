eb27cae312968d2c3a7b2092d8a4a6139c097899
Robert Munroe <54950744+Robert-Munroe@users.noreply.github.com>
2019-11-09 14:25:21 -0500

    Add files via upload
    This is for part C-1. The message properly displays here. I have a ton of comments to make it easier to read. I'll add a script for compiling load file. Load file will run when ever you call ./compileOS.sh

---
+++ compileOS.sh
@@ -0,0 +1,8 @@
+
+dd if=/dev/zero of=diskc.img bs=512 count=1000
+dd if=bootload of=diskc.img bs=512 count=1 conv=notrunc
+bcc -ansi -c -o kernel_c.o kernel.c
+as86 kernel.asm -o kernel_asm.o
+ld86 -o kernel -d kernel_c.o kernel_asm.o
+./loadFile kernel
+./loadFile message.txt
