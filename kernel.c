2bede8de7c81a0afa7618611f5f12ff244100c3f
Robert Munroe <54950744+Robert-Munroe@users.noreply.github.com>
2019-12-04 18:43:58 -0500

    Add files via upload
    These are my steps for project - D part one, starting part two now.

--- kernel.c
+++ kernel.c
@@ -3,42 +3,66 @@
 	void readString(char*); 	//readString function declaration
 	void readSector(char); 		// read sector
 	void handleInterrupt21(int, int, int, int); //handle's interrupt 21
-	void readFile(char*,char*,int*);
-	void executeProgram(char*);
-	void terminate();
+	void readFile(char*,char*,int*);	// read file
+	void executeProgram(char*);		// execute program
+	void terminate();			//terminate
+	void writeSector();			//write sector
 
-void main() {
+void main() {//main
 
-	makeInterrupt21();
-	interrupt(0x21, 4, "shell", 0, 0);
-	while(1);
-}
+	makeInterrupt21();			//creates our interrupt handler
+	interrupt(0x21, 4, "shell", 0, 0);	//uses the interrupt handler and runs shell
+	while(1);				// fail safe
+}//end of main
 
-void terminate(){
-	char shellname[6];
-	shellname[0]='s';
+void writeSector(){//write sector
+
+	//parameter set up
+	int AH = 2;
+	int AL = 1;
+	int BX = buffer;
+	int CH = 0;
+	int CL = sector + 1;
+	int DH = 0;
+	int DL = 0x80;
+
+	//more parameter set up
+	int AX = 6;
+	int CX = CH*256+CL;
+	int DX = DH*256+DL;
+
+	//interrupt
+	interrupt(0x13, AX, BX, CX, DX);
+}//end of write sector
+
+void terminate(){//terminate
+
+	char shellname[6];	// creates char array
+	shellname[0]='s'; 	//loads shell into it
 	shellname[1]='h';
 	shellname[2]='e';
 	shellname[3]='l';
 	shellname[4]='l';
 	shellname[5]='\0';
 
-	executeProgram(shellname);
-}
+	executeProgram(shellname);	//executes shellname
+}//end of terminate
 
-void executeProgram(char* name) {
-	int i;
-	int sectorsRead;
-	char buffer[13312];
+void executeProgram(char* name) {//execute program
 
-	readFile(name, buffer, &sectorsRead);
-	if(sectorsRead == 0){return;}
-	for(i=0; i<13312; i++) {
-		putInMemory(0x2000,i,buffer[i]);
-	}
+	int i;			// integer
+	int sectorsRead;	// sector read
+	char buffer[13312];	// buffer of 13312
 
-	launchProgram(0x2000);
-}
+	readFile(name, buffer, &sectorsRead);//read file
+
+	if(sectorsRead == 0){return;}		//if sector is zero, leave
+	for(i=0; i<13312; i++) {		//if not
+		putInMemory(0x2000,i,buffer[i]);//put into memory untill i is 13311
+	}//end of if not
+
+	launchProgram(0x2000);//launch program in 0x2000
+}//end of execute program
 
 void readFile(char* name, char* buffer, int* sectorsRead){
 	int i = 0;
@@ -47,7 +71,7 @@ void readFile(char* name, char* buffer, int* sectorsRead){
 	char dir[512];
 	*sectorsRead = 0;
 	interrupt(0x21,2,dir,2,0);
-	for(i = 0; i <512; i=i+32){
+	for(i = 0; i <512; i=i+32){//start of for loop 1
 	//printChar(dir[i]);
 	//printChar(dir[i+1]);
 	//printChar(dir[i+2]);
@@ -55,29 +79,34 @@ void readFile(char* name, char* buffer, int* sectorsRead){
 	//printChar(dir[i+4]);
 	//printChar(dir[i+5]);
 	//printChar('h');
-		if(name[0] == dir[i+0]&&name[1] == dir[i+1] &&name[2] == dir[i+2] &&name[3] == dir[i+3] &&name[4] == dir[i+4] &&name[5] == dir[i+5]){
+		if(name[0] == dir[i+0]&&
+			name[1] == dir[i+1] &&
+			name[2] == dir[i+2] &&
+			name[3] == dir[i+3] &&
+			name[4] == dir[i+4] &&
+			name[5] == dir[i+5]){ //if
 			*sectorsRead = 1;
-			for(j =i+ 6; dir[j]!=0; j+=32){
+
+			for(j =i+ 6; dir[j]!=0; j+=32){//for loop 2
 				interrupt(0x21,2,buffer,dir[j],0);
  				buffer = buffer + 512;
-			}
-		}
-	
-	}
-}
+			}//end of for loop 2
+		}//end of if
+	}//end of for loop 1
+}//end of read file
 
-void printChar(char c) {
+void printChar(char c) {//print char
         char al = c;
         interrupt(0x10,0xe*256+al,0,0,0);
-}
+}//end of print char
 
-void printString(char* chars) {
+void printString(char* chars) {//print string
 	int i=0;
-	while (chars[i] != 0x0) {
+	while (chars[i] != 0x0) {//while
 		printChar(chars[i]);
 		i++;
-	}
-}
+	}//end of while
+}//end of print string
 
 void readString(char* chars) {
 	int i = 0;
@@ -136,9 +165,11 @@ void handleInterrupt21(int ax, int bx, int cx, int dx) {//start of handle 21
 	if(ax==5){
 		terminate();
 	}
+	if(ax==6){
+		writeSector();
+	}
 
 }//end of handle 21
-			
 
 void readSector(char*buffer ,int sector){// start of readSector
 
@@ -150,7 +181,7 @@ void readSector(char*buffer ,int sector){// start of readSector
 	int CL = sector+1;
 	int DH = 0;
 	int DL = 0x80;
-	
+
 	// more parameter set up (simple input)
 	int AX = AH*256+AL;
 	int CX = CH*256+CL;
