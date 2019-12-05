396d0b6dad3836c8f57c104ba3bf97282c2ddb64
Robert Munroe <54950744+Robert-Munroe@users.noreply.github.com>
2019-12-04 19:23:09 -0500

    Add files via upload
    organized and added a simple test version for dir. all it does currently is tell me it is a valid command. it prints systemcall 0, "dir is working\n\r"

--- shell.c
+++ shell.c
@@ -1,4 +1,4 @@
-main(){
+main(){//start of main
 
 	char line[80];
 	char command[10];
@@ -11,45 +11,58 @@ main(){
 	syscall(1, line); //takes input
 
 
-		if((line[0] == 't' &&
-			 line[1] == 'y' &&
-			 line[2] == 'p' &&
-			 line[3] == 'e') ||
+	if((line[0] == 't' &&
+		 line[1] == 'y' &&
+		 line[2] == 'p' &&
+		 line[3] == 'e') ||
 
-			 (line[0] == 'e' &&
-			 line[1] == 'x' &&
-			 line[2] == 'e' &&
-			 line[3] == 'c')){
+		 (line[0] == 'e' &&
+		 line[1] == 'x' &&
+		 line[2] == 'e' &&
+		 line[3] == 'c') ||
 
-			while(line[i] != '\0'){
-				command[j] = line[i];
-				i++;
-				j++;
-			}
-			if(line[0] == 't' &&
-			 line[1] == 'y' &&
-			 line[2] == 'p' &&
-			 line[3] == 'e'){
-				syscall(3, command, buffer, &sectorsRead);
+		(line[0] == 'd' &&
+		line[1] == 'i' &&
+		line[2] == 'r')){ //command if
 
-			if(sectorsRead > 0){
-					syscall(0, buffer);
-				}
-				else{
-					syscall(0,"File not found\n\r");
-				}
-			}
-			if(line[0] == 'e' &&
-			 line[1] == 'x' &&
-			 line[2] == 'e' &&
-			 line[3] == 'c'){
-				syscall(4, command);
-					syscall(0, "exe file not found\n\r");
-			}
+		while(line[i] != '\0'){//while
+			command[j] = line[i];
+			i++;
+			j++;
+		}//end of while
 
+		if(line[0] == 't' &&
+		 line[1] == 'y' &&
+		 line[2] == 'p' &&
+		 line[3] == 'e'){//type if
+			syscall(3, command, buffer, &sectorsRead);
+
+		if(sectorsRead > 0){//read sector if
+				syscall(0, buffer);
+			}//end of read sector if
+			else{//read sector else
+				syscall(0,"File not found\n\r");
+			}//end of read sector else
+		}//end of type if
+
+		if(line[0] == 'e' &&
+		line[1] == 'x' &&
+	 	line[2] == 'e' &&
+	 	line[3] == 'c'){//execute command
+			syscall(4, command);
+			syscall(0, "exe file not found\n\r");
+		}//end of execute command
+
+		if(line[0] == 'd'&&
+		line[1] == 'i'&&
+		line[2] == 'r'){//dir command
+			syscall(0, "dir is working\n\r");
 		}
-		else{
-			syscall(0, "Bad Command\n\r");
-		}
-	}
+	}//end of command if
+
+	else{//command else
+		syscall(0, "Bad Command\n\r");
+	}//end of command else
+
+}//end of shell main
 
